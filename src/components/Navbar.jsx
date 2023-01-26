6d0b935341c731ecb1d66bba5e6050d81e6b2a4e
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-26 13:08:02 +0100

    navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/airbnb-logo.png";
+
+function Nav() {
+  return (
+    <div>
+      <nav className="nav">
+        <img src={logo}></img>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
