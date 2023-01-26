275e2bcc062f5fd9195644526c3d4aceb1238d57
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-26 13:07:14 +0100

    components import

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,14 @@
+import Nav from "./components/Navbar";
+import Hero from "./components/Hero";
 import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
+
 export default App;
